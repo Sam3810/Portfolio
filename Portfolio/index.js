d387363b5bd842650da445b543393750bf16f15e
cocinero <118696492+Sam3810@users.noreply.github.com>
2023-05-06 22:50:08 -0500

    Add files via upload

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,5 +1,15 @@
 let button = document.getElementById('proyecto__button')
 let github = document.getElementById('footer__button')
+let submit = document.getElementById('input__enviar')
+let nombre = document.getElementById('input__nombre')
+let correo = document.getElementById('input__correo')
+let mensaje = document.getElementById('input__mensaje')
+
+
+
+
+
+
 
 button.addEventListener('click', webpage)
 github.addEventListener('click', githubpage)
@@ -11,3 +21,12 @@ function webpage () {
 function githubpage () {
     window.open('https://github.com/Sam3810')
 }
+
+submit.addEventListener('click', function() {
+
+    if (nombre.value !== "" && correo.value !== "" && mensaje.value !== "") {
+        alert('Correo enviado exitosamente')
+    }
+    
+});
+
