39b1466ed0f0f590b0991d12981fb9c8443f2b42
cocinero <118696492+Sam3810@users.noreply.github.com>
2023-05-15 14:28:35 -0500

    Add files via upload

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,31 +1,5 @@
-let button = document.getElementById('proyecto__button')
-let button2  = document.getElementById('proyecto2__button')
-let github = document.getElementById('footer__button')
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 let interval = null;
-
-
-
-
-button.addEventListener('click', page1)
-button2.addEventListener('click', page2)
-github.addEventListener('click', githubpage)
-
-function page1 () {
-    window.open('https://orianabuenoduque.netlify.app/')
-}
-
-function page2 () {
-    window.open('https://bethoven.netlify.app/')
-}
-
-function githubpage () {
-    window.open('https://github.com/Sam3810')
-}
-
-
-
-
 
 
 document.querySelector("h1").onmouseover = event => {  
